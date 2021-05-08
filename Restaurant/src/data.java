72715a7035e74f6077adc68edd20166c957ad494
Fahri <arirahman236@gmail.com>
2021-05-08 18:09:45 +0700

    open pdf

---
+++ Restaurant/src/data.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Fahri
+ */
+public interface data {
+    public void tampilMinuman();
+    public void tampilMakanan();
+    public void kembalian();
+    public void tampilTotal();
+    
+}
